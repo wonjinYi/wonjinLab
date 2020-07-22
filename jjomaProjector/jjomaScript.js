6588757c54f343dccd3ec01abd949f920da057b3
wonjinYi <studioplug17@gmail.com>
2020-07-22 15:10:12 +0000

    now just rendering is remained

--- jjomaProjector/jjomaScript.js
+++ jjomaProjector/jjomaScript.js
@@ -21,30 +21,55 @@ async function main(){
 	const SUBMITTED_LIST = await separateRowFromJson(SOURCE.submitted, COLUMNS.submitted);
 	const ASSIGNMENT_TYPE = await separateRowFromJson(SOURCE.assignment_type, COLUMNS.assignment_type);
 	
-	let data = [];
-	
 	console.log(SUBMITTED_LIST);
 	console.log(ASSIGNMENT_TYPE);
 	
-	let tmp = getSubCategory(ASSIGNMENT_TYPE, CATEGORY_KEY, "스네이크");
-	console.log(tmp);
+	let data = {};
 	
-	let str = '';
-	for(let i=0; i<ASSIGNMENT_TYPE.length; i++){
-		//str += makeStructuerdString(ASSIGNMENT_TYPE[i], SUBMITTED_LIST[i]);
-	}
-	
+	//카테고리 구조화
 	const ROOT_CATEGORY_SET = getRootCategory(ASSIGNMENT_TYPE, CATEGORY_KEY);
 	for(let i=0; i<ROOT_CATEGORY_SET.length; i++){
-		
 		const subCategory = getSubCategory(ASSIGNMENT_TYPE, CATEGORY_KEY, ROOT_CATEGORY_SET[i]);
 		categorizeItems(data, SUBMITTED_LIST, CATEGORY_KEY, ROOT_CATEGORY_SET[i], subCategory);
+	}
+	
+	//html스트링 만들기
+	for(let i=0; i<ROOT_CATEGORY_SET.length; i++){
+		makeHtmlString( ROOT_CATEGORY_SET[i] , (data[ROOT_CATEGORY_SET[i]]) );
 	}
 	
 	console.log(data);
 	//TARGET['article'].innerHTML = str;
 	
 	
+}
+
+
+function makeHtmlString(mainCategory, assignment){
+	let subCategoryLength = (Object.keys(assignment)).length
+	// html 구문 만들기
+	let tmp = ''; // 해당 subjectType안에 리스트가 하나라도 있는지 확인하기 위한 초기문자열
+	
+	let str = '';
+	str += 	'<div id="'+mainCategory+'" class="type-container">'
+	str += 		'<h2 class="type">'+mainCategory+'</h2>';
+	tmp = str;
+	
+	for(let i=0; i<subCategoryLength; i++){
+		if(subCategoryLength!=1){
+			str += '<a class="subTitle">'+assignment
+		}
+		str += 		'<ul>';
+	
+	
+	
+	
+	str += 		'</ul>';
+	}
+	
+	str += 		'</div>';
+
+	return str;
 }
 
 /*
@@ -70,20 +95,17 @@ function categorizeItems(data, source, CATEGORY_KEY, mainCategory, subCategory  
 	
 	// _data[] 객체에 서브카테고리 속성 추가
 	for(let i=0; i<subCategory.length; i++){
-		data[ mainCategory ][ subCategory[i] ] = {
+		data[ mainCategory ][ subCategory[i].categoryName ] = {
+			description : subCategory[i].description,
 			items : []
 		};
 	}
 	
-	//console.log(data[mainCategory]);
-	
 	// 각 서브카테고리에 해당하는 row를, item속성에 넣어주기
 	for(let i=0; i<source.length; i++){
-		//console.log("test : "+i);
 		for(let k=0; k<subCategory.length; k++){
-			
-			if(source[i][CATEGORY_KEY.sub] == subCategory[k]){
-				( (data[ mainCategory ][ subCategory[k] ]).items ).push( source[i] );
+			if(source[i][CATEGORY_KEY.sub] == subCategory[k].categoryName ){
+				( (data[ mainCategory ][ subCategory[k].categoryName ]).items ).push( source[i] );
 				break;
 			}
 			
@@ -92,37 +114,6 @@ function categorizeItems(data, source, CATEGORY_KEY, mainCategory, subCategory  
 	
 }
 
-function makeStructuerdString(category, arr){
-	// html 구문 만들기
-	let tmp = ''; // 해당 subjectType안에 리스트가 하나라도 있는지 확인하기 위한 초기문자열
-	
-	//임시로 사용.
-	let subjectType = category;
-	console.log(subjectType);
-	console.log(arr);
-	
-	let str = '';
-	str += 	'<div id="'+subjectType+'" class="type-container">'
-	str += 		'<h2 class="type">'+subjectType+'</h2>';
-	str += 		'<ul>';
-	tmp = str;
-	for(let i=0; i<arr.length; i++){
-		if(arr[i]["큰유형"] == subjectType){
-			str += 	'<li class="list">';
-			str +=		'<a class="title" href="'+arr[i]['서비스url']+'" target="_blank">'+arr[i]['제목']+'</a>';
-			str += 		' by '+arr[i]['제출자'];
-			
-			if(arr[i]['메모'] != ''){str += ' "'+arr[i]['메모']+'"';}
-			str +=	'</li>';
-		}
-	}
-	if(str == tmp){ return ''; } // 해당 과제유형에 제출된과제 하나도없으면 과제유형 안보여줌
-	
-	str += 		'</ul>';
-	str += 		'</div>';
-
-	return str;
-}
 
 async function separateRowFromJson(url, columns){
 
@@ -148,7 +139,7 @@ function getRootCategory(source, CATEGORY_KEY){
 	}
 	
 	tmp = Array.from(new Set(tmp));
-	console.log("=====getroot : "+tmp);
+	
 	return tmp;
 }
 
@@ -156,9 +147,8 @@ function getSubCategory(category, CATEGORY_KEY, mainCategoryValue){
 	let subCategory = [];
 	
 	for(let i=0; i<category.length; i++){
-		
 		if(category[i][CATEGORY_KEY.main] == mainCategoryValue){
-			subCategory.push(category[i][CATEGORY_KEY.sub]);
+			subCategory.push({ categoryName : category[i][CATEGORY_KEY.sub], description : category[i]['설명'] });
 		}
 	}
 	
