d672d00c4e2f82842000a66a6638dc8a997db468
wonjinYi <studioplug17@gmail.com>
2020-08-11 15:05:07 +0000

    it can display cards.

--- GoesTop/assign02/goestopScript.js
+++ GoesTop/assign02/goestopScript.js
@@ -1,18 +1,31 @@
 window.addEventListener('DOMContentLoaded', main);
 
-const CARDS = [	"1_G", "1_T", "1_P1", "1_P2",
-				"2_Y", "2_T", "2_P1", "2_P2",
-				"3_G", "3_T", "3_P1", "3_P2",
-				"4_Y", "4_T", "4_P1", "4_P2",
-				"5_Y", "5_T", "5_P1", "5_P2",
-				"6_Y", "6_T", "6_P1", "6_P2",
-				"7_Y", "7_T", "7_P1", "7_P2",
-				"8_G", "8_Y", "8_P1", "8_P2",
-				"9_Y", "9_T", "9_P1", "9_P2",
-				"10_Y", "10_T", "10_P1", "10_P2",
-				"11_G", "11_PP", "11_P1", "11_P2",
-				"12_G", "12_Y", "12_T", "12_PP",
-				"X_PP1", "X_PP2", "X_PPP" ];
+const CARDS = [	
+	"1_G", "1_T", "1_P1", "1_P2",
+	"2_Y", "2_T", "2_P1", "2_P2",
+	"3_G", "3_T", "3_P1", "3_P2",
+	"4_Y", "4_T", "4_P1", "4_P2",
+	"5_Y", "5_T", "5_P1", "5_P2",
+	"6_Y", "6_T", "6_P1", "6_P2",
+	"7_Y", "7_T", "7_P1", "7_P2",
+	"8_G", "8_Y", "8_P1", "8_P2",
+	"9_Y", "9_T", "9_P1", "9_P2",
+	"10_Y", "10_T", "10_P1", "10_P2",
+	"11_G", "11_PP", "11_P1", "11_P2",
+	"12_G", "12_Y", "12_T", "12_PP",
+	"X_PP1", "X_PP2", "X_PPP" 
+];
+
+
+let cardPool = [];
+CARDS.forEach( card => { cardPool.push(card); } );
+
+const PLAYERS_NUM = 2; // The number of game players
+
+//Options for card alignment
+const LINE_BREAK_NUM = 10
+const OVERLAP_SPACE = { horizon:30, vertical:40 };
+
 
 
 function main(){
@@ -20,17 +33,18 @@ function main(){
 		body : document.getElementsByTagName('body'),
 		module : document.getElementsByClassName('test-module'),
 	};
+	
+	let players = initPlayers(PLAYERS_NUM);
 	let drawNum = 10;
 	
-	let myCards = {
-		G : [],
-		Y : [],
-		T : [],
-		P : []
+	for(let i=0; i<PLAYERS_NUM; i++){
+		organizeCards(players[i].cards, drawCard(drawNum, i) );
+		//console.log((players[i].container.cards).getElementsByClassName('G'));
+		updateCardContainer(players[i]);
 	}
+	console.log(players);
+	//
 	
-	organizeCards(myCards, drawCard(drawNum), TARGET);
-	updateCardContainer(myCards);
 	
 	//temp button for test -------------------------------------
 	/*
@@ -49,33 +63,76 @@ function main(){
 
 }
 
+function initPlayers(PLAYERS_NUM){
+	const root = document.getElementsByClassName('test-module');
+	let _players = [];
+	
+	for(let i=0; i<PLAYERS_NUM; i++){
+		let player = {
+			container:{},
+			cards:{}
+		};
+		
+		player.container.status = root[i].getElementsByClassName('status-main-container')[0];
+		player.container.cards = root[i].getElementsByClassName('card-main-container')[0];
+		player.cards = {	
+			G : [],
+			Y : [],
+			T : [],
+			P : []
+		};
+		
+		_players.push(player);
+	}
+	
+	return _players;
+}
 
-function organizeCards(myCards, drawn, TARGET){
+function drawCard(num, PLAYER_INDEX){ // 'draw' means 'select a random card'. NOT DRAWING A PAINT!
+	let drawnCards = [];
+	
+	for(let i=0; i<num; i++){
+		while(true){
+			const index = Math.floor(Math.random() * cardPool.length);
+			const card = cardPool[index];
+			if(drawnCards.includes(card) == false){
+				drawnCards.push(card);
+				cardPool.splice(index,1);
+				break;
+			}	
+		}
+	}
+	
+	return drawnCards;
+}
+
+function organizeCards(playerCards, drawnCards){
+	//const KEYS = ['G', 'Y', 'T', 'P'];
+	
 	//classitication 
-	for(let i=0; i<drawn.length; i++){
-		for(let key in myCards){
-			if(drawn[i].includes(key)){
-				myCards[key].push(drawn[i]);
+	for(let i=0; i<drawnCards.length; i++){
+		for(let key in playerCards){
+			if(drawnCards[i].includes(key)){
+				playerCards[key].push(drawnCards[i]);
 				break;
 			}
 		}
 	}
 }
 
-function updateCardContainer(myCards){
-	//make HTML elements by classified cards ( "myCards" Object )
-	const LINE_BREAK_NUM = 10
-	const OVERLAP_SPACE = {horizon:30, vertical:40};
+function updateCardContainer(player){
+	const CARDS = player.cards;
+	const CARDS_CONTAINER = player.container.cards;
 	
-	for(let key in myCards){
-		
+	for(let key in CARDS){
+		//make HTML elements from classified cards ( "players[i].cards" Object )
 		let str = '';
-		for(let i=0; i<myCards[key].length; i++){
+		for(let i=0; i<CARDS[key].length; i++){
 			const rest = i%LINE_BREAK_NUM;
 			
 			if(rest == 0){str += '<div class="cards-line">';}
 			
-			str += '<img src="../card_img/'+myCards[key][i]+'.png" ';
+			str += '<img src="../card_img/'+CARDS[key][i]+'.png" ';
 			str += 		'class="card" ';
 			str +=		'style="left:-'+(OVERLAP_SPACE.horizon* (i%LINE_BREAK_NUM) )+'px;';
 			str +=				'position:relative;';
@@ -83,53 +140,36 @@ function updateCardContainer(myCards){
 			str += '>';
 			
 			if(rest == 9){str += '</div>';}
-			else if (i==myCards[key].lenth-1){str += '</div>';}
+			else if (i==CARDS[key].lenth-1){str += '</div>';}
 		}
 		
 		
 		// If there are cards in current Class(key), update the Element.
 		if(str!=''){
+			CARDS_CONTAINER.getElementsByClassName(key)[0].innerHTML = str;
+			
+			// set negative margin-right of .cards class
+				// If there is line breaking, Its margin is max value ( = LINE_BREAK_NUM * OVERLAP_SPACE )
+				// BUT, there is not line breaking, margin is determined by the number of element(card)
 			let _marginRight = 0;
+			if(CARDS[key].length >= LINE_BREAK_NUM){ _marginRight = (-1) * LINE_BREAK_NUM * OVERLAP_SPACE.horizon; }
+			else { _marginRight = (-1) * ( (player.cards[key].length)%LINE_BREAK_NUM ) * OVERLAP_SPACE.horizon; }
+			CARDS_CONTAINER.getElementsByClassName(key)[0].style.marginRight = _marginRight + 'px';
+		
+			// Vertical Overlap  ::  If there is line Breaking, overlap .card-line element vertically
+			if(CARDS[key].length>LINE_BREAK_NUM){
+				const CARDS_LINE = ( CARDS_CONTAINER.getElementsByClassName(key)[0] ).getElementsByClassName('cards-line');
+
+				for(let i=0; i<CARDS_LINE.length - 1; i++ ){
+					CARDS_LINE[i].style.top = ( (CARDS_LINE.length - 1 - i) * OVERLAP_SPACE.vertical ) + 'px';
+				}
+			}
 			
-			// set negative margin of .cards class.
-			// If there were line breaking, these margin is max value ( = LINE_BREAK_NUM * OVERLAP_SPACE )
-			// BUT, there were not that, margin is determined by the number of element(card)
-			if(myCards[key].length >= LINE_BREAK_NUM){_marginRight = (-1) * LINE_BREAK_NUM * OVERLAP_SPACE.horizon;}
-			else {_marginRight = (-1) * ( (myCards[key].length)%LINE_BREAK_NUM ) * OVERLAP_SPACE.horizon; }
-			
-			document.getElementById(key).innerHTML = str;
-			document.getElementById(key).style.marginRight = _marginRight + 'px';
 		}
 		else{
-			document.getElementById(key).innerHTML = '<div class="dummy"></div>';
-		}
-		
-		// If there were line Breaking, overlap .card-line element vertically
-		if(myCards[key].length>LINE_BREAK_NUM){
-			const CARDS_LINE = ( document.getElementById(key) ).getElementsByTagName('div');
-
-			for(let i=0; i<CARDS_LINE.length - 1; i++ ){
-				CARDS_LINE[i].style.top = ( (CARDS_LINE.length - 1 - i) * OVERLAP_SPACE.vertical ) + 'px';
-			}
-		}
-		
-	}
-}
-
-function drawCard(num){ // 'draw' means 'select a random card'. NOT DRAWING A PAINT!!!!!
-	let drawnCards = [];
-	
-	for(let i=0; i<num; i++){
-		while(true){
-			let index = Math.floor(Math.random() * CARDS.length);
-			if(drawnCards.includes(CARDS[index]) == false){
-				drawnCards.push(CARDS[index]);
-				break;
-			}	
+			CARDS_CONTAINER.getElementsByClassName(key)[0].innerHTML = '<div class="dummy"></div>';
 		}
 	}
-	
-	return drawnCards;
 }
 
 function range(start, end){
@@ -140,4 +180,3 @@ function range(start, end){
 	
 	return arr;
 }
-
