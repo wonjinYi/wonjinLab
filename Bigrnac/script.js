5de77031092332ff19c17e91f93c2b0bc6022b94
wonjinYi <studioplug17@gmail.com>
2020-09-12 22:16:51 +0000

    bug fix. reported by tnraro

--- Bigrnac/script.js
+++ Bigrnac/script.js
@@ -1,52 +1,56 @@
 window.addEventListener('DOMContentLoaded', main);
 
-const TARGET={
-	body : document.getElementsByTagName('body'),
-	blocks : document.getElementsByClassName('block'),
-};
+let TARGET;
 let clone;
+let isDraggingStarted = false;
 
 async function main(){
-	let topSum = 0;
+	TARGET = {
+		body : document.getElementsByTagName('body'),
+		blockContainer : document.getElementById('block-container'),
+		blocks : document.getElementsByClassName('block'),
+	};
 	
+	// block setting
+	let topSum = 0;
 	Array.prototype.forEach.call( TARGET.blocks, block => {
 		block.style.top = topSum + 'px';
 		topSum += block.offsetHeight;
 		
-		document.addEventListener('mousedown',startDrag);
-		document.addEventListener('mouseup',endDrag);
+		TARGET.blockContainer.addEventListener('mousedown',startDrag);
+		TARGET.blockContainer.addEventListener('mouseup',endDrag);
 	});
+	
+	// change block.style.top when screen is resized
+	window.addEventListener('resize', relocateBlocks);
 
 	
 }
 
 function startDrag(e){
+	e.preventDefault()
+	
+	if (isDraggingStarted == true) { return 0; }
+	else { isDraggingStarted = true; }
+	
 	const selectedElement = e.target;
 	console.log('start drag', selectedElement);
 	
 	clone = document.createElement('div');
-	clone.style.top = e.clientY + 'px';
+	clone.style.top = selectedElement.style.top;
 	clone.style.zIndex = 53;
-	selectedElement.classList.forEach( className => {
-		console.log(className);
-		clone.classList.add(className);
-	});
+	selectedElement.classList.forEach( className => clone.classList.add(className) );
 	selectedElement.classList.forEach( className => selectedElement.classList.remove(className) );
-	
-	
 	selectedElement.classList.add("block", "empty");
 	
+	TARGET.blockContainer.insertBefore(clone, selectedElement);
 	
-	TARGET.body[0].insertBefore(clone, selectedElement);
-	
-	document.addEventListener('mousemove', moveBlock);
+	TARGET.blockContainer.addEventListener('mousemove', moveBlock);
 }
 
 function moveBlock(e){
-	console.log('MOVE MOVE', e.target, e.clientX, e.clientY);
-	//e.target
+	//console.log('MOVE MOVE', e.target, e.clientX, e.clientY);
 	clone.style.top = e.clientY + 'px';
-	//e.target.style.left = e.clientX + 'px';
 }
 
 function endDrag(e){
@@ -56,7 +60,18 @@ function endDrag(e){
 	selectedElement.classList.remove('empty');
 	clone.classList.forEach( className => selectedElement.classList.add(className) );
 	clone.classList.forEach( className => clone.classList.remove(className) );
-	
 	clone.remove();
-	document.removeEventListener('mousemove', moveBlock);
+	
+	TARGET.blockContainer.removeEventListener('mousemove', moveBlock);
+	
+	isDraggingStarted = false;
+}
+
+function relocateBlocks(e){
+	console.log('resize');
+	let topSum = 0;
+	Array.prototype.forEach.call( TARGET.blocks, block => {
+		block.style.top = topSum + 'px';
+		topSum += block.offsetHeight;
+	});
 }
