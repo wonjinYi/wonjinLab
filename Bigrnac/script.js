2332bbea3e75e592e9a3ddfb2d8a26d3ec21a81b
wonjinYi <studioplug17@gmail.com>
2020-09-15 14:56:29 +0000

    generate resizebars. clean up code.

--- Bigrnac/script.js
+++ Bigrnac/script.js
@@ -10,8 +10,10 @@ let AUDIOS = {
 }
 let ELEMENT = {};
 let BLOCKS = [];
+let RESIZEBARS = [];
 let target = 0; // For swap : the destination where a clone returns.
 				// For row resize : upper element.
+let MODE = ''; // swap  OR  row-resize
 let clone; // html element
 let isDraggingStarted = false;
 let oldMouseY = 0;
@@ -32,6 +34,17 @@ async function main () {
 		topSum += BLOCKS[i].offsetHeight;
 	}
 	
+	// create Resizing bars
+	topSum = 0;
+	for ( let i=0; i<BLOCKS.length-1; i++ ) {
+		RESIZEBARS.push( document.createElement('div') );
+		RESIZEBARS[i].classList.add('resize-bar');
+		
+		ELEMENT.blockContainer.insertBefore(RESIZEBARS[i], BLOCKS[i+1]);
+		
+		RESIZEBARS[i].style.top = ( BLOCKS[i+1].offsetTop - parseInt(RESIZEBARS[i].offsetHeight/2) ) + 'px';
+	}
+	
 	// warn
 	if( confirm('wanna play Audio?\n소리 재생 ㄱ?') == false ) { AUDIOS = {}; }
 	
@@ -40,117 +53,126 @@ async function main () {
 	ELEMENT.blockContainer.addEventListener('touchstart',startDrag);
 	ELEMENT.blockContainer.addEventListener('mouseup',endDrag);
 	ELEMENT.blockContainer.addEventListener('touchend',endDrag);
+	
 	window.addEventListener('resize', relocateBlocks);
-
-	
 }
 
-function selectResizeLine(e){
-	const block = e.target;
-	
-	//아래부분 마우스오버
-	if (e.clineY > block.offsetTop + block.offsetHeight*0.95 ){
-		console.log()
-	}
-	//윗부분 마우스오버
-	else if(e.target.offsetTop)
-
-}
 
 function startDrag (e) {
 	e.preventDefault()
 	
-	if (window.innerWidth > 769) { oldMouseY = e.clientY; }
-	else { oldMouseY = e.touches[0].clientY; }
-	
 	if (isDraggingStarted == true) { return 0; }
 	else { isDraggingStarted = true; }
 	
-	// make clone
-	target = parseInt( (e.target.dataset).index );
-	//console.log('start drag', BLOCKS[dest]);
-	
-	clone = document.createElement('div');
-	clone.style.top = BLOCKS[target].style.top;
-	clone.style.zIndex = 53;
-	clone.style.borderRadius = "100px";
-	
-	BLOCKS[target].classList.forEach( className => clone.classList.add(className) ); // copy e.target -> clone
-	BLOCKS[target].className = '';
-	BLOCKS[target].classList.add("block", "empty");
-	
-	ELEMENT.blockContainer.insertBefore(clone, BLOCKS[target]);
-	ELEMENT.blockContainer.addEventListener('mousemove', moveBlock);
-	ELEMENT.blockContainer.addEventListener('touchmove', moveBlock);
-	
-	(Object.keys(AUDIOS)).forEach( audio => {
-		if ( clone.classList.contains(audio) ){
-			AUDIOS[audio].play();
-			return;
-		}
-	})
+	if (window.innerWidth > 769) { oldMouseY = e.clientY; }
+	else { oldMouseY = e.touches[0].clientY; }
 	
 	
-	//console.log(BLOCKS[dest].dataset.index);
+	// SWAP =====================================================================================
+	if( BLOCKS.includes( e.target ) ) {
+		
+		MODE = 'swap';
+		
+		// make clone
+		target = parseInt( (e.target.dataset).index );
+
+		clone = document.createElement('div');
+		clone.style.top = BLOCKS[target].style.top;
+		clone.style.zIndex = 53;
+		clone.style.borderRadius = "100px";
+
+		BLOCKS[target].classList.forEach( className => clone.classList.add(className) ); // copy e.target -> clone
+		BLOCKS[target].className = '';
+		BLOCKS[target].classList.add("block", "empty");
+
+		ELEMENT.blockContainer.insertBefore(clone, BLOCKS[target]);
+		ELEMENT.blockContainer.addEventListener('mousemove', moveBlock);
+		ELEMENT.blockContainer.addEventListener('touchmove', moveBlock);
+
+		(Object.keys(AUDIOS)).forEach( audio => {
+			if ( clone.classList.contains(audio) ){
+				AUDIOS[audio].play();
+				return;
+			}
+		})
+	}
+	
+	// RESIZE ==============================================================================
+	else if( RESIZEBARS.includes( e.target ) ) {
+		MODE = 'row-resize';
+		alert('resize bar - mouse down');
+	}
 }
 
 function moveBlock(e) {
 	
-	//console.log('move ', e.clientY, oldMouseY,e.touches[0].clientY);
-	
-	let newPosition = 0;
-	if (window.innerWidth > 769) { 
-		clone.style.top = (clone.offsetTop + ( e.clientY - oldMouseY )) + 'px';
-		oldMouseY = e.clientY; 
-	}
-	else { 
-		clone.style.top = (clone.offsetTop + ( e.touches[0].clientY - oldMouseY )) + 'px';
-		oldMouseY = e.touches[0].clientY; 
-	}
+	// SWAP =====================================================================================
+	if( MODE == 'swap' ) {
+		let newPosition = 0;
+		if (window.innerWidth > 769) { 
+			clone.style.top = (clone.offsetTop + ( e.clientY - oldMouseY )) + 'px';
+			oldMouseY = e.clientY; 
+		}
+		else { 
+			clone.style.top = (clone.offsetTop + ( e.touches[0].clientY - oldMouseY )) + 'px';
+			oldMouseY = e.touches[0].clientY; 
+		}
 
+
+		// 밑으로 보내기
+		if ( target < BLOCKS.length-1 ) {
+			if ( clone.offsetTop + clone.offsetHeight > centerOf(BLOCKS[target+1]) ) {
+				swapClassList( BLOCKS[target], BLOCKS[target+1] );
+				target++;
+			}
+		}
+		// 위로 보내기
+		if ( target > 0 ) {
+			if ( clone.offsetTop < centerOf(BLOCKS[target-1]) ) {
+				swapClassList( BLOCKS[target], BLOCKS[target-1] );
+				target--;
+			}
+		}
+	}
 	
-	// 밑으로 보내기
-	if ( target < BLOCKS.length-1 ) {
-		if ( clone.offsetTop + clone.offsetHeight > centerOf(BLOCKS[target+1]) ) {
-			swapClassList( BLOCKS[target], BLOCKS[target+1] );
-			target++;
-		}
+	// RESIZE ==============================================================================
+	else if( MODE = 'row-resize' ) {
+		
 	}
-	// 위로 보내기
-	if ( target > 0 ) {
-		if ( clone.offsetTop < centerOf(BLOCKS[target-1]) ) {
-			swapClassList( BLOCKS[target], BLOCKS[target-1] );
-			target--;
-		}
-	}
+	
 }
 
 
 function endDrag(e) {
-	console.log('END drag', BLOCKS[target]);
 	
-	// remove clone
-	BLOCKS[target].classList.remove('empty');
-	clone.classList.forEach( className => BLOCKS[target].classList.add(className) ); // copy clone -> e.target
-	clone.className = '';
-	console.log('remove clone classlist ', clone.classList);
-	clone.remove();
+	// SWAP =====================================================================================
+	if( MODE == 'swap' ) {
+		// remove clone
+		BLOCKS[target].classList.remove('empty');
+		clone.classList.forEach( className => BLOCKS[target].classList.add(className) ); // copy clone -> e.target
+		clone.className = '';
+		console.log('remove clone classlist ', clone.classList);
+		clone.remove();
+
+		ELEMENT.blockContainer.removeEventListener('mousemove', moveBlock);
+		ELEMENT.blockContainer.removeEventListener('touchmove', moveBlock);
+
+		// initial status check
+		// <!-- 빵 패티 소스 양상추 빵 패티 치즈 소스 양상추 빵 -->
+		const kkiro = ['bun', 'patty', 'sauce', 'lettuce', 'bun', 'patty', 'cheese', 'sauce', 'lettuce', 'bun'];
+		for(let i=0; i<BLOCKS.length; i++){
+			if(BLOCKS[i].classList.contains(kkiro[i]) == false){break;}
+			else { if(i==BLOCKS.length-1){ AUDIOS['full'].play(); } }
+		}
+	}
 	
-	ELEMENT.blockContainer.removeEventListener('mousemove', moveBlock);
-	ELEMENT.blockContainer.removeEventListener('touchmove', moveBlock);
-	
-	// initial status check
-	// <!-- 빵 패티 소스 양상추 빵 패티 치즈 소스 양상추 빵 -->
-	const kkiro = ['bun', 'patty', 'sauce', 'lettuce', 'bun', 'patty', 'cheese', 'sauce', 'lettuce', 'bun'];
-	for(let i=0; i<BLOCKS.length; i++){
-		if(BLOCKS[i].classList.contains(kkiro[i]) == false){break;}
-		else { if(i==BLOCKS.length-1){ AUDIOS['full'].play(); } }
+	// RESIZE ==============================================================================
+	else if( MODE = 'row-resize' ) {
+
 	}
 	
 	
 	isDraggingStarted = false;
-
-	
 }
 
 
